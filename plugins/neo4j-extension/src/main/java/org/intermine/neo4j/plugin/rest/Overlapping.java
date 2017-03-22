df72d089ada20bc81b6fd0699b3e446502940635
danielabutano <daniela@intermine.org>
2017-03-22 14:04:00 +0000

    caught exception when gene has no symbol

--- plugins/neo4j-extension/src/main/java/org/intermine/neo4j/plugin/rest/Overlapping.java
+++ plugins/neo4j-extension/src/main/java/org/intermine/neo4j/plugin/rest/Overlapping.java
@@ -195,7 +195,11 @@ public class Overlapping
         Gene gene = new Gene();
         gene.setPrimaryIdentifier((String) node.getProperty("primaryidentifier"));
         gene.setSecondaryIdentifier((String) node.getProperty("secondaryidentifier"));
-        gene.setSymbol((String) node.getProperty("symbol"));
+        try {
+            gene.setSymbol((String) node.getProperty("symbol"));
+        } catch (NotFoundException nfe) {
+            log.info("the node " + (String) node.getProperty("primaryidentifier") + " doesn't have a symbol");
+        }
         List<Gene> overlappingGenes = new ArrayList<Gene>();
         for (Relationship overlaps : node.getRelationships(IntermineRelationships.OVERLAPS, Direction.BOTH)) {
             Node overlappingGeneNode = overlaps.getOtherNode(node);
